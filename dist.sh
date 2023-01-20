4143a3ff7ec58ecb9547b4c653e6a8bc4a79f032
Jihee Ryu <chrisjihee@etri.re.kr>
2023-01-20 11:59:48 +0900

    update dist.sh for sleep

--- dist.sh
+++ dist.sh
@@ -4,5 +4,6 @@ pip install build; python3 -m build;
 pip install twine; python3 -m twine upload dist/*;
 rm -rf build dist src/*.egg-info;
 
+sleep 3s; clear;
 conda create --name chrislab python=3.10 -y; conda activate chrislab;
 pip install --upgrade chrislab; pip list;
