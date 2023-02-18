fd2b7f921474fc69a9749bec1fdce1dac8d07c03
Jihee Ryu <chrisjihee@etri.re.kr>
2023-02-18 12:03:49 +0900

    Version 0.2.2: using updated MyTimer

--- dist.sh
+++ dist.sh
@@ -1,10 +1,10 @@
-conda create --name chrislab python=3.10 -y; conda activate chrislab;
+conda create --name chrislab python=3.9 -y; conda activate chrislab;
 rm -rf build dist src/*.egg-info;
 pip install build; python3 -m build;
 pip install twine; python3 -m twine upload dist/*;
 rm -rf build dist src/*.egg-info;
 
 sleep 3s; clear;
-conda create --name chrislab python=3.10 -y; conda activate chrislab;
+conda create --name chrislab python=3.9 -y; conda activate chrislab;
 sleep 5s; clear;
 pip install --upgrade chrislab; pip list;
