7f1b253e51ac054f6c8e6a9aa0d377b48bf4b043
Jihee Ryu <chrisjihee@etri.re.kr>
2023-04-25 23:39:34 +0900

    Update train_ner(): - Implement remained part: 교재를 참고하여 ratsnlp 이용한 NER 학습 구현!

--- src/chrislab/ratsnlp/cli.py
+++ src/chrislab/ratsnlp/cli.py
@@ -12,7 +12,10 @@ from ratsnlp.nlpbook.classification.arguments import ClassificationTrainArgument
 from ratsnlp.nlpbook.classification.corpus import NsmcCorpus, ClassificationDataset
 from ratsnlp.nlpbook.classification.deploy import get_web_service_app
 from ratsnlp.nlpbook.classification.task import ClassificationTask
-from transformers import BertConfig, BertForSequenceClassification
+from ratsnlp.nlpbook.ner.arguments import NERTrainArguments
+from ratsnlp.nlpbook.ner.corpus import NERCorpus, NERDataset
+from ratsnlp.nlpbook.ner.task import NERTask
+from transformers import BertConfig, BertForSequenceClassification, BertForTokenClassification
 from transformers import BertTokenizer
 
 app = Typer()
@@ -21,6 +24,76 @@ app = Typer()
 @app.command()
 def train_ner(config: Path | str):
     config = Path(config)
+    assert config.exists(), f"No config file: {config}"
+    args = NERTrainArguments.from_json(config.read_text())
+    args.print_dataframe()
+
+    with JobTimer(f"chrialab.ratsnlp train_ner {config}",
+                  mt=1, mb=1, rt=1, rb=1, rc='=', verbose=True, flush_sec=0.3):
+        nlpbook.set_seed(args)
+        nlpbook.set_logger()
+        out_hr(c='-')
+
+        nlpbook.download_downstream_dataset(args)
+        out_hr(c='-')
+
+        tokenizer = BertTokenizer.from_pretrained(
+            args.pretrained_model_path,
+            do_lower_case=False,
+        )
+        print(f"tokenizer={tokenizer}")
+        print(f"tokenized={tokenizer.tokenize('안녕하세요. 반갑습니다.')}")
+        out_hr(c='-')
+
+        corpus = NERCorpus(args)
+        train_dataset = NERDataset(
+            args=args,
+            corpus=corpus,
+            tokenizer=tokenizer,
+            mode="train",
+        )
+        train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=args.batch_size,
+            sampler=RandomSampler(train_dataset, replacement=False),
+            collate_fn=nlpbook.data_collator,
+            drop_last=False,
+            num_workers=args.cpu_workers,
+        )
+        out_hr(c='-')
+
+        val_dataset = NERDataset(
+            args=args,
+            corpus=corpus,
+            tokenizer=tokenizer,
+            mode="val",
+        )
+        val_dataloader = DataLoader(
+            val_dataset,
+            batch_size=args.batch_size,
+            sampler=SequentialSampler(val_dataset),
+            collate_fn=nlpbook.data_collator,
+            drop_last=False,
+            num_workers=args.cpu_workers,
+        )
+        out_hr(c='-')
+
+        pretrained_model_config = BertConfig.from_pretrained(
+            args.pretrained_model_path,
+            num_labels=corpus.num_labels,
+        )
+        model = BertForTokenClassification.from_pretrained(
+            args.pretrained_model_path,
+            config=pretrained_model_config,
+        )
+        out_hr(c='-')
+
+        torch.set_float32_matmul_precision('high')
+        nlpbook.get_trainer(args).fit(
+            NERTask(model, args),
+            train_dataloaders=train_dataloader,
+            val_dataloaders=val_dataloader,
+        )
 
 
 @app.command()
@@ -30,7 +103,7 @@ def train_cls(config: Path | str):
     args = ClassificationTrainArguments.from_json(config.read_text())
     args.print_dataframe()
 
-    with JobTimer(f"chrialab.ratsnlp train {config}",
+    with JobTimer(f"chrialab.ratsnlp train_cls {config}",
                   mt=1, mb=1, rt=1, rb=1, rc='=', verbose=True, flush_sec=0.3):
         nlpbook.set_seed(args)
         nlpbook.set_logger()
