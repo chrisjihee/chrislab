7d22863547bb900bb32210b031e2df3610e80be3
Jihee Ryu <chrisjihee@etri.re.kr>
2023-10-23 16:51:45 +0900

    Update nlpbook.dp.corpus.py: import

--- src/nlpbook/dp/corpus.py
+++ src/nlpbook/dp/corpus.py
@@ -1,5 +1,7 @@
-import re
+from __future__ import annotations
+
 import logging
+import re
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List, Optional, Dict, ClassVar
